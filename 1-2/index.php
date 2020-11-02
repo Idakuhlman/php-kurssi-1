c5704804bd7e8cc31466324099ec0f24377ef453
Idakuhlman <73815943+Idakuhlman@users.noreply.github.com>
2020-11-02 22:22:24 +0200

    update 1-1\index.php
    Hello World

--- 1-2/index.php
+++ 1-2/index.php
@@ -1,1 +1,16 @@
 <?php
+$letters = "Hello";
+$number = 589;
+$decimal = 50.95;
+$table = ["A", "B", "C"];
+
+echo $letters;
+echo "<br>";
+echo $number;
+echo "<br>";
+echo $decimal;
+echo "<br>";
+print_r($table);
+
+
+
